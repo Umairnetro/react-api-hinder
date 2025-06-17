0ce8d7191a706811a569981630b8d7d081c88212
Umair Ahmed <umairjdu@gmail.com>
2025-06-18 00:22:16 +0500

    check

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   const api = process.env.REACT_APP_API_URL || "Hello, There is some problem";
   const ViteApi =
-    process.env.Vite_APP_API_URL || "I am Vite Checker, There is some problem";
+    import.meta.env.VITE_API_URL || "I am Vite Checker, There is some problem";
 
   return (
     <>
