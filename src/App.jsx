9f200435c7469d5cfff36f3f0e2b521aa626ae85
Umair Ahmed <umairjdu@gmail.com>
2025-06-18 00:26:14 +0500

    env update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 
 function App() {
-  const api = process.env.REACT_APP_API_URL || "Hello, There is some problem";
   const ViteApi =
     import.meta.env.VITE_API_URL || "I am Vite Checker, There is some problem";
 
   return (
     <>
       <section>
-        <p>This is my greeting: {`${api}`}</p>
         <p>This is my Vite greeting: {`${ViteApi}`}</p>
       </section>
     </>
